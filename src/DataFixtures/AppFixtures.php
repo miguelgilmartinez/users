c2f2bd5ad821d643cfe21f20db73a35551b14b86
MiguelGil <miguelgilmartinez@gmail.com>
2021-07-17 00:49:09 +0200

    Added fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,26 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\PatientUsers;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+use Symfony\Component\Uid\Uuid;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $user = new User();
+        $user->setEmail('yeah@slsl.com');
+        $user->setSagaStatus("NEW");
+        $user->setUsername("userzzz");
+        $user->setPhoneNumber("+380665555555");
+        $patient = new PatientUsers;
+        $patient->setUser($user);
+        $patient->setPatientUuid(Uuid::v4());
+        $user->addPatientUser($patient);
+        $manager->persist($user);
+        $manager->persist($patient);
         $manager->flush();
     }
 }
